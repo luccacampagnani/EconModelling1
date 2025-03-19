d8909c2a4c7e08e2c229478e4d412a6e50e1ad22
luccacampagnani <lcampagnani.ieu2023@student.ie.edu>
2025-03-19 16:41:38 +0100

    session 3 ECONOMIC MODELLING

--- color_point.py
+++ color_point.py
@@ -1,26 +1,37 @@
 from point import Point
 import random
 
+class PointException(Exception):
+    pass
+
 class ColorPoint(Point):
     def __init__(self, x, y, color):
-        self.x = x
-        self.y = y
+        if not isinstance(x, (int, float)):
+            raise TypeError("x must be a number")
+        if not isinstance(y, (int, float)):
+            raise TypeError("y must be an number")
+
+        super().__init__(x, y)
         self.color = color
 
     def __str__(self):
         return f"<{self.color}: {self.x}, {self.y}>"
 
+#p = ColorPoint(1, 2, "red")
+#print(p)
+#colors = ["red", "green", "blue", "yellow", "black", "magenta"]
+
+#color_point = []
+#for i in range(10):
+#    color_point.append(
+#        ColorPoint(random.randint(-10, 10),
+#                   random.randint(-10, 10),
+#                   random.choice(colors)))
+
+#print(color_point)
+#color_point.sort()
+#print(color_point)
+
 p = ColorPoint(1, 2, "red")
+print(p.distance_origin())
 print(p)
-colors = ["red", "green", "blue", "yellow", "black", "magenta"]
-
-color_point = []
-for i in range(10):
-    color_point.append(
-        ColorPoint(random.randint(-10, 10),
-                   random.randint(-10, 10),
-                   random.choice(colors)))
-
-print(color_point)
-color_point.sort()
-print(color_point)
